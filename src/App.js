5e6a92ce59443ea8503346eecc534e5934c6d3b2
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-05-31 16:37:53 +0530

    fetch data through api using async await

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movie,setMovies] = useState([])
+ async function fetchMovies() {
+  const response = await fetch('https://swapi.dev/api/films/')
+  
+  const data = await response.json()
+    const transformedData =  data.results.map(movie=>{
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date
+
+
+      }
+    })
+    console.log(data)
+    
+   setMovies(transformedData)}
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovies}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movie} />
+      </section>
+    </React.Fragment>
   );
 }
 
